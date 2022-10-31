922dcd05224e7910996db99ec21bf66b9bbb0f4a
Bobby <pfsystem.brown@gmail.com>
2022-10-30 19:17:04 -0500

    init

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -1,31 +1,59 @@
-import { generatorHandler, GeneratorOptions } from '@prisma/generator-helper'
-import { logger } from '@prisma/sdk'
-import path from 'path'
-import { GENERATOR_NAME } from './constants'
-import { genEnum } from './helpers/genEnum'
-import { writeFileSafely } from './utils/writeFileSafely'
+import { generatorHandler, GeneratorOptions } from "@prisma/generator-helper";
+import { logger } from "@prisma/sdk";
+import { parseEnvValue } from "@prisma/internals";
+import * as path from "path";
+import { GeneratorPathNotExists } from "./error-handler";
+import { GENERATOR_NAME } from "./constants";
+import { genEnum } from "./helpers/genEnum";
+import { writeFileSafely } from "./utils/writeFileSafely";
 
-const { version } = require('../package.json')
+const { version } = require("../package.json");
+
+export const PrismaClassGeneratorOptions = {
+    makeIndexFile: {
+        desc: "make index file",
+        defaultValue: true
+    },
+    dryRun: {
+        desc: "dry run",
+        defaultValue: true
+    },
+    separateRelationFields: {
+        desc: "separate relation fields",
+        defaultValue: false
+    },
+    useSwagger: {
+        desc: "use swagger decorator",
+        defaultValue: true
+    }
+} as const;
+
+export type PrismaClassGeneratorOptionsKeys =
+    keyof typeof PrismaClassGeneratorOptions;
+export type PrismaClassGeneratorConfig = Partial<
+    Record<PrismaClassGeneratorOptionsKeys, any>
+>;
 
 generatorHandler({
-  onManifest() {
-    logger.info(`${GENERATOR_NAME}:Registered`)
-    return {
-      version,
-      defaultOutput: '../generated',
-      prettyName: GENERATOR_NAME,
+    onManifest() {
+        logger.info(`${GENERATOR_NAME}:Registered`);
+        return {
+            version,
+            defaultOutput: "../generated",
+            prettyName: GENERATOR_NAME
+        };
+    },
+    onGenerate: async (options: GeneratorOptions) => {
+        options.dmmf.datamodel.enums.forEach(async (enumInfo) => {
+            const tsEnum = genEnum(enumInfo);
+            logger.info(`${GENERATOR_NAME}:enumget:${tsEnum}`);
+
+            const writeLocation = path.join(
+                options.generator.output?.value!,
+                `${enumInfo.name}.ts`
+            );
+            logger.info(` writing to ${writeLocation}`);
+            await writeFileSafely(writeLocation, tsEnum);
+        });
     }
-  },
-  onGenerate: async (options: GeneratorOptions) => {
-    options.dmmf.datamodel.enums.forEach(async (enumInfo) => {
-      const tsEnum = genEnum(enumInfo)
-
-      const writeLocation = path.join(
-        options.generator.output?.value!,
-        `${enumInfo.name}.ts`,
-      )
-
-      await writeFileSafely(writeLocation, tsEnum)
-    })
-  },
-})
+});
